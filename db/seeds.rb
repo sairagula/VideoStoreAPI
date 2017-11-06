542241ee016edc97ce4cc7f794a2c79cb7a93911
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-06 12:28:53 -0800

    Initial set up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
-  Customer.create!(customer)
-end
-
-JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
-  Movie.create!(movie)
-end
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)

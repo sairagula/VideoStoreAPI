6bba4828ed69bd518f6e7413788ef66af4858513
Sairagul Abdukhalieva <abdukhalieva@gmail.com>
2017-11-06 19:51:22 -0800

    Model teting for Movie relationships

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,45 @@
+class MoviesController < ApplicationController
+
+  def index
+    movies = Movie.all
+    render(
+      json: movies.as_json(only: [:id, :title, :overview, :release_date, :inventory]),
+      status: :ok
+    )
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie
+      render(
+        json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]),
+        status: :ok
+      )
+    else
+      render(
+        json: {nothing: true}, status: :not_found
+      )
+    end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render(
+        json: {id: movie.id}, status: :ok
+      )
+    else
+      render(
+        json: {errors: movie.errors.messages}, status: :bad_request
+      )
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+  end
+end
---
+++ test/controllers/movies_controller_test.rb
@@ -0,0 +1,110 @@
+require "test_helper"
+
+
+describe MoviesController do
+
+  describe "index" do
+    it "is a real working route" do
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_path
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = ["id", "inventory", "overview", "release_date", "title" ]
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
+
+    it "returns an empty array if there are no movies" do
+      # Arrange
+      Movie.destroy_all
+
+      # Act
+      get movies_path
+
+      # Assert
+      must_respond_with :success
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+      body.must_be :empty?
+    end
+  end
+
+  describe "show" do
+    # This bit is up to you!
+    it "can get a movie" do
+      get movie_path(movies(:two).id)
+      must_respond_with :success
+    end
+
+    it "responds correctly when movie is not found" do
+      invalid_movie_id = Movie.all.last.id + 1
+      get movie_path(invalid_movie_id)
+
+      must_respond_with :not_found
+
+      body = JSON.parse(response.body)
+      body.must_equal "nothing" => true
+    end
+  end
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "Cool movie",
+        inventory: 7,
+        release_date: "feb",
+        overview: "Something"
+      }
+    }
+
+
+    it "Create a Movie" do
+      proc {
+        post movies_path, params: {movie: movie_data}
+      }.must_change 'Movie.count', 1
+
+      must_respond_with :success
+    end
+
+    it "won't change the db if given invalid data" do
+      invalid_movie_data =
+        {
+          title: nil,
+          inventory: 7,
+          release_date: "feb",
+          overview: "Something"
+        }
+      proc {
+        post movies_path, params: {movie: invalid_movie_data}
+      }.wont_change 'Movie.count'
+
+      must_respond_with :bad_request
+
+      body = JSON.parse(response.body)
+      body.must_equal "errors" => {"title" => ["can't be blank"]}
+    end
+  end
+end

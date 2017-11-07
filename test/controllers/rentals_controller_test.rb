6bc172ceb98b39822c7407fed95e97afb53a5e31
Tamira Vojnar <tamiravojnar@gmail.com>
2017-11-07 14:17:19 -0800

    finished test for create

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe RentalsController do
   describe "checkout" do
     it "will create a new instance of rental" do
+      # arrange
       c = Customer.first
       m = Movie.first
       rental_data =
@@ -10,11 +11,18 @@ describe RentalsController do
         customer_id: c.id,
         movie_id: m.id
       }
-      start_count = Rental.count
 
+      start_count = Rental.count
+      availible = m.available_inventory
+      num_movies = c.movies_checked_out_count
+
+      # Act
       post rentals_path, params: {rental: rental_data}
 
+      # Assert
       Rental.count.must_equal start_count + 1
+      m.available_inventory.must_equal availible - 1
+      c.movies_checked_out_count.must_equal num_movies + 1 
     end # create a new instance
   end # checkout
 end
